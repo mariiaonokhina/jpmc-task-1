d28bb0e6bd23b5835560fe0b2501d5f83b96bd94
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-02-05 10:29:24 -0500

    Fixed ratios between stocks

--- client.py
+++ client.py
@@ -31,18 +31,22 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # Price of a stock is the avg of bid price and ask price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    # Guardian pattern to make sure no division by 0 happens
+    if price_b == 0:
+        return
+    
+    # Return the ration of price_a and price_b
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +55,15 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        prices = dict()   # Created a dictionary to store prices
+        
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            
+            # Map stock name to price
+            prices[stock] = price
+            
+            # Changed formatting of printing making it easier to read
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        print(f"Ratio {getRatio(prices['ABC'], prices['DEF'])}")
